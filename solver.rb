0535797a8160abdb30a2752b5b326f81dc096183
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-03 12:20:34 -0500

    Implement factorial method

--- solver.rb
+++ solver.rb
@@ -1,1 +1,9 @@
+class Solver
+  def factorial(number)
+    raise ArgumentError, 'Factorial is not defined for negative integers.' if number.negative?
 
+    result = 1
+    (1..number).each { |i| result *= i }
+    result
+  end
+end
