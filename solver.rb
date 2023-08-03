40ef0a3679f3d24a7b9fe19773a8ffb78b95a91b
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-03 12:21:20 -0500

    Implement fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -10,4 +10,16 @@ class Solver
   def reverse(word)
     word.reverse
   end
+
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s # Convert the number to a string
+    end
+  end
 end
